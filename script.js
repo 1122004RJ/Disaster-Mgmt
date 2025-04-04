4644e57e9169cf55b75d71567b15dfd4d0e6fb33
Rishabh Jain <47220916+1122004RJ@users.noreply.github.com>
2025-04-04 12:06:24 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function showAlerts() {
+    alert("⚠️ No new emergency alerts at the moment.");
+}
+
+function openMap() {
+    window.open("https://www.google.com/maps", "_blank");
+}
+
+function checkResources() {
+    alert("🛠️ Emergency resources are currently available in your area.");
+}
